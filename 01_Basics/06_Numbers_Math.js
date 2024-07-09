f916dea6a7f3e53995f5c43443fafce1be356042
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 23:45:01 +0530

    Math and Numbers

---
+++ 01_Basics/06_Numbers_Math.js
@@ -0,0 +1,41 @@
+const score = 400
+
+const balance = new Number(100)
+
+
+// console.log(score);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 23.34534
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  // en-In Indian Standard Number Format
+
+
+
+// ++++++++++++++++++++++   MATHS   +++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);                  
+// console.log(Math.abs(-4));              //=> 4
+// console.log(Math.round(35.6));          //=> 36
+// console.log(Math.ceil(4.2));            //=> 5
+// console.log(Math.floor(4.9));           //=> 4
+// console.log(Math.sqrt(25));             //=> 5 Square Root 
+// console.log(Math.min(4,3,2,1));         //=> 1
+
+// console.log(Math.random());                 // Value between 0 and 1
+console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1))) + min)
+
+
+
