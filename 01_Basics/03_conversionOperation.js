848e51b97aeee5468f9e3e4ffd906635c0e7743e
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 18:39:58 +0530

    operations and prefix postfix

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "333"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -22,8 +22,8 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true and 0 => false
 // "" => false
@@ -33,4 +33,48 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+// ************* Operations *************// 
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " tanay"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+
+// prefix and postfix operations 
+
+let a = 3
+let b = ++a
+
+// console.table([a, b])
+
+
