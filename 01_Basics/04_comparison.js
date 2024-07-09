e0537c701015653ba54958203ad47de2a9461a24
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 18:53:32 +0530

    comparison in js

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(1 > 2);  => false
+// console.log(2 >= 1); => false
+// console.log(2 < 1);  => false
+// console.log(2 == 1); => flase
+// console.log(2 != 1); => true
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+
+/// === Strict Check
+
+console.log("2" === 2);
