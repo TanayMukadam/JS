b01d6fceeaeab1202691abec302ed5d0e7c7df64
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 16:18:06 +0530

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3 + 3) we are using nodeks, not browser
+
+// console.log(3 + 3);
+
+// console.log("Tanay");
+
+
+// Data Types
+
+let name = "tanay"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// numer => 2 to power 53
+// bigint
+// string =>  ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+console.log(typeof undefined);
+console.log(typeof null);
