613709835c21b0f66f8adbe0e241e9bc5e5b263d
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 21:01:52 +0530

    DataTypes Summary

---
+++ 01_Basics/05_dataTypesSummary.js
@@ -0,0 +1,49 @@
+// Primitive Data Types
+
+// 7 Categories : String, Number, Boolean, Null, Undefined, Symbol and BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+
+console.log(id === anotherId);
+
+const BigNumber =  187234892137489237489n   // Big Int add n at the back
+
+console.log(typeof BigNumber);
+// Reference (Non Premitive)
+
+/* 1. Array
+   2. Objects
+   3. Functions */
+
+// 1. Arrays []
+const heros = ["IronMan", "CaptainAmerica", "Thor"];
+
+console.log(heros);
+
+// 2. Objects {}
+
+let myObj = {
+    name: "tanay",
+    age: 23
+}
+
+console.log(myObj);
+
+
+// 3. Functions 
+
+const myFun = function() {
+    console.log("Hello World")
+}
+
+
+console.log(typeof heros);
