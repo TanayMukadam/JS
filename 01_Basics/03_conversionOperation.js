8042ac37eb6074219bfbf1ac7e60468cedfd5c1b
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 16:36:53 +0530

    conversion operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "333"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+
+// "33abc" => Nan (Not a Number)
+
+// true => 1 and False => 0
+
+// null => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true and 0 => false
+// "" => false
+// "tanay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
