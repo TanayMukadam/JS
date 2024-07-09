a55316d4461cb3db100f3c18af6bf735442bd2b3
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-09 15:57:24 +0530

    Constants, Variables and Let

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "tanaymukadam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Thane"
+let accountState;
+// accountId = 2
+
+accountEmail = "hiyagusai@gmail.com"
+accountPassword = "212321"
+accountCity = "Pune"
+
+// console.log(accountId);
+
+/*
+let is preferred more by developers than var because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail, accountId, accountCity, accountState])
