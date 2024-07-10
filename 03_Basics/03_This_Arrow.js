9578e9bc71607d3e9708b71abbe9eda7fba4f253
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-10 15:43:23 +0530

    Arrow Function in JS

---
+++ 03_Basics/03_This_Arrow.js
@@ -0,0 +1,47 @@
+const user = {
+    username: "tanay",
+    price: 999,
+
+    welcomeMessage: function () {
+        // console.log(`${this.username} , welcome to website`);
+        // console.log(this);
+    }
+}
+
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+
+
+///////////////// +++++++ Arrow Function +++++++++++++++//////////////
+
+
+
+// function one () {
+
+//     let username = "tanay"
+//     console.log(this.username);
+// }
+// one()
+
+
+// const one = () => {
+//     let username = "tanay"
+//     console.log(this);
+// }
+
+// one()
+
+
+// () => {}    Arrow Function
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+
+
