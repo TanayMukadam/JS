da71b4102be8993abb76543e227f8a5c712e762b
Tanay Mukadam <tanaymukadam@gmail.com>
2024-07-10 12:07:57 +0530

    Modified Arrays Part2

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -1,31 +1,29 @@
-const marvel_heros = ["Thor", "IronMan", "Spider-Man"]
-const dc = ["SuperMan", "Flash", "BatMan"]
+const marvel_heros = ["Thor", "IronMan", "Spider-Man"];
+const dc = ["SuperMan", "Flash", "BatMan"];
 
 // console.log(marvel_heros);
 
-let heros = marvel_heros.concat(dc)
+let heros = marvel_heros.concat(dc);
 
 // console.log(heros[3]);
 
+// Spread Operator
 
-// Spread Operator 
-
-const all_new_heros = [...marvel_heros, ...dc]
+const all_new_heros = [...marvel_heros, ...dc];
 // console.log(all_new_heros);
 
-
-const another_array = [1,2,3, [4,5,6], 7, [6,7], [4, 5]]
-const real_another_array = another_array.flat(Infinity)
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7], [4, 5]];
+const real_another_array = another_array.flat(Infinity);
 // console.log(another_array);
 // console.log(real_another_array);
 
 console.log(Array.isArray("Tanay"));
 console.log(Array.from("Tanay"));
 
-console.log(Array.from({name: "Tanay"}));
+console.log(Array.from({ name: "Tanay" }));
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
 
 console.log(Array.of(score1, score2, score3));
